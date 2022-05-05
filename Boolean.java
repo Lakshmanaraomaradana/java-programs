5ae8ca8a780da602791fba519c9f12c071d5c354
Lakshmanaraomaradana <104361307+Lakshmanaraomaradana@users.noreply.github.com>
2022-05-05 20:39:07 +0530

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Boolean
+  {
+    public static void main(String args[])
+    {
+      Scanner b=new Scanner(System.in);
+      boolean t;
+      System.out.println("enter boolean value=");
+      t=b.nextBoolean();
+      if(t==true)
+      System.out.println("false");
+      
+    }
+  }
