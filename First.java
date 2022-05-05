fb10dbff78100780d4002d84668d4e31a1352c14
Lakshmanaraomaradana <104361307+Lakshmanaraomaradana@users.noreply.github.com>
2022-05-05 20:45:38 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,16 @@
+class First
+  {
+    public static void main(String args[])
+    {
+      int a=101;
+      String b="KARUN";
+      float c=458.56f;
+      double d=46.56;
+      char f='h';
+      System.out.println("the given employee number is :"+a);
+      System.out.println("the given employee name is :"+b);
+      System.out.println("the given employee salary is:"+c);
+      System.out.println("the given employee is :"+d);
+      System.out.println("the given charecter is:"+f);
+    }
+  }
