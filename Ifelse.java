37e13744050cd97b5e2827f9986396acd6733b8d
Lakshmanaraomaradana <104361307+Lakshmanaraomaradana@users.noreply.github.com>
2022-05-05 20:47:42 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Ifelse
+  {
+    public static void main(String args[])
+    {
+      Scanner b=new Scanner(System.in);
+      int a;
+      System.out.println("enter nuber a=");
+      a=b.nextInt();
+      if(a>=1);      
+      {
+        System.out.println("entered number is positive number");
+        }
+        
+      else
+      {
+        
+        System.out.println("entered number is nagative");
+      }
+    }
+  }
+  
