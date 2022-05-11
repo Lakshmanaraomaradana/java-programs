faa495f0310854ae17dd663e77602811116830ce
Lakshmanaraomaradana <104361307+Lakshmanaraomaradana@users.noreply.github.com>
2022-05-11 11:18:19 +0530

    Add files via upload

---
+++ Factorialtwo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Factorialtwo
+  {
+    public static void main(String args[])
+    {
+      Scanner sn=new Scanner(System.in);
+      int i=o,n,a=n;
+      
+      System.out.println("enter n value:");
+      n=sn.nextInt();
+      while(i<=1)
+        {
+          d;
+          i--;
+        }
+      System.out.println("the factorial of n natural numbers :"+sum);
+      
+    }
+  }
