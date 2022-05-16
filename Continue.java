49f5fd95a3c67ce53fcf56127f062fd84e7961a6
Lakshmanaraomaradana <104361307+Lakshmanaraomaradana@users.noreply.github.com>
2022-05-16 11:46:14 +0530

    Add files via upload

---
+++ Continue.java
@@ -0,0 +1,16 @@
+class Continue
+  {
+    public static void main(String args[])
+    {
+      int i;
+      for(i=1;i<=10;i++)
+        {
+          if(i==6||i==7)
+          {
+            continue;
+          }
+          System.out.println(i);
+        }
+     
+    }
+  }
