f5549eb69e7ce5fabec2e11d6f30738fde00f65f
Lakshmanaraomaradana <104361307+Lakshmanaraomaradana@users.noreply.github.com>
2022-05-17 10:06:15 +0530

    Add files via upload

---
+++ Integer.java
@@ -0,0 +1,19 @@
+class Integer
+  {
+    public static void main(String args[])
+    {
+      int i=9,n=300,sum=0;
+      for(i=9;i<=300;i++)
+        {
+          
+          if(i%7==0&&i%63!=0)
+          {
+            sum=sum+i;
+          }
+          
+          }
+          System.out.println("the sum of integers are : "+sum);
+          
+    
+  }
+  }
