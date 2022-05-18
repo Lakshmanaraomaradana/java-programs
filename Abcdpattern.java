27fa26f470d36f84aa85809d877170f339c5fdfd
Lakshmanaraomaradana <104361307+Lakshmanaraomaradana@users.noreply.github.com>
2022-05-18 10:29:39 +0530

    Add files via upload

---
+++ Abcdpattern.java
@@ -0,0 +1,18 @@
+class Abcdpattern
+  {
+    public static void main (String args[])
+    {
+      int i,j,ch=65;
+        for(i=1;i<=5;i++)
+          {
+            for(j=1;j<=5;j++)
+              {
+              System.out.print((char)(ch));
+                ch++;
+                }
+              System.out.println(" "); 
+          }
+     
+      
+    }
+  }
