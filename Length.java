ca2370b372cdadccf4ca699582b65ab466986e36
Lakshmanaraomaradana <104361307+Lakshmanaraomaradana@users.noreply.github.com>
2022-05-20 15:55:17 +0530

    Add files via upload

---
+++ Length.java
@@ -0,0 +1,10 @@
+class Length
+  {
+    public static void main(String args[])
+    {
+      String st1="welcome";
+      int len=st1.length();
+      System.out.println("the length of the string is:"+len);
+      
+    }
+  }
