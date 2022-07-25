1fe65a63ac40ea3723013cced1990abc59defa50
Lakshmanaraomaradana <104361307+Lakshmanaraomaradana@users.noreply.github.com>
2022-07-25 15:09:59 +0530

    Add files via upload

---
+++ Natural1ton.java
@@ -0,0 +1,13 @@
+class Natural1ton
+  {
+    public static void main(String args[])
+    {
+      int i=1,n=10;
+      System.out.println("1 to 10 numbers are:");
+      while (i<=n)
+        {
+          System.out.println(i);
+          i++;
+        }
+    }
+  }
